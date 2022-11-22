24ac0adff7c79dfbed22ad8d50f827337c04648d
Ryszard Gregory <ryszard1c@gmail.com>
2022-11-22 20:30:40 +0100

    Connection Scoped

--- JokeManagment/Server/ConnectionSQL.cs
+++ JokeManagment/Server/ConnectionSQL.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace JokeManagment.Server
-{
+{//Connection Scoped
     public static class ConnectionSQL
     {
         static string stringConnection = "User ID = kbdqewln; Password = 0sz7xXfaP49UwO41tkddPpniCQr2b7DL; Host = surus.db.elephantsql.com; Port = 5432; Database = kbdqewln;";
